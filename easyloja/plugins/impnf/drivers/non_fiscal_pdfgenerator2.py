70cdf488c5c61481d23b4dd3b150fda1d316a7c1
marcelo <mmaia.cc@gmail.com>
2018-02-09 10:11:39 -0300

    VErsão nova do PDF

--- easyloja/plugins/impnf/drivers/non_fiscal_pdfgenerator2.py
+++ easyloja/plugins/impnf/drivers/non_fiscal_pdfgenerator2.py
@@ -1,11 +1,10 @@
+# coding=utf-8
+from reportlab.lib.enums import TA_LEFT, TA_CENTER
 from reportlab.lib.styles import ParagraphStyle as PS
 from reportlab.lib.units import mm
-from reportlab.platypus import PageBreak
-from reportlab.lib.enums import TA_LEFT, TA_CENTER, TA_RIGHT, TA_JUSTIFY
 from reportlab.platypus.doctemplate import PageTemplate, BaseDocTemplate
 from reportlab.platypus.frames import Frame
 from reportlab.platypus.paragraph import Paragraph
-from reportlab.platypus.tableofcontents import TableOfContents
 
 from stoqlib.reporting.base.flowables import ReportLine
 
@@ -21,16 +20,6 @@ class MyDocTemplate(BaseDocTemplate):
                                 [Frame(left_margin, right_margin, width_doc, height_doc, id='F1')],
                                 pagesize=(width_doc, height_doc))
         self.addPageTemplates(template)
-
-    # Entries to the table of contents can be done either manually by
-    # calling the addEntry method on the TableOfContents object or automatically
-    # by sending a 'TOCEntry' notification in the afterFlowable method of
-    # the DocTemplate you are using. The data to be passed to notify is a list
-    # of three or four items countaining a level number, the entry text, the page
-    # number and an optional destination key which the entry should point to.
-    # This list will usually be created in a document template's method like
-    # afterFlowable(), making notification calls using the notify() method
-    # with appropriate data.
 
     def afterFlowable(self, flowable):
         "Registers TOC entries."
@@ -55,6 +44,14 @@ h1_centered = PS(
     leading=12,
     alignment=TA_CENTER,
 )
+
+h1_left = PS(
+    name='Heading1Left',
+    fontSize=10,
+    leading=12,
+    alignment=TA_LEFT,
+)
+
 
 h2_centered = PS(
     name='Heading2Center',
@@ -89,33 +86,25 @@ footer = PS(
 
 # Build story.
 story = []
-
-# Create an instance of TableOfContents. Override the level styles (optional)
-# and add the object to the story
-
-# toc = TableOfContents()
-# toc.levelStyles = [
-#     PS(fontName='Times-Bold', fontSize=12, name='TOCHeading1', leftIndent=20, firstLineIndent=-20, spaceBefore=10,
-#        leading=16),
-#     PS(fontSize=18, name='TOCHeading2', leftIndent=40, firstLineIndent=-20, spaceBefore=5, leading=12),
-# ]
-# story.append(toc)
-
-story.append(Paragraph('<b>EMPRESA BRASILEIRA DE INFORMATICA LTDA ME SA...</b>', h1_centered))
-story.append(Paragraph('AVENIDA JOSE BONIFACIO, 352, BELEM-PA', h1_centered))
-story.append(Paragraph('(91)3226-5660', h1_centered))
+story.append(Paragraph('<b>EMPRESA BRASILEIRA DE INFORMATICA LTDA ME SA</b>', h1_centered))
+story.append(Paragraph('AVENIDA JOSE BONIFACIO, 352, BELEM-PA', h1_left))
+story.append(Paragraph('Fone: (91)3226-5660', h1_left))
+story.append(Paragraph('CNPJ: 83.325.498/0001-90 IE154698031', h1_left))
 story.append(ReportLine())
 story.append(Paragraph('<b>CUPOM NAO FISCAL</b>', header_warning))
 story.append(ReportLine())
 story.append(Paragraph('<b>|COD|Descricao                              |QTD|X|VlrUnit|VlrTotal</b>'
                        .replace(' ', '&nbsp;'), header_items))
 story.append(ReportLine())
-for i in range(100):
+for i in range(15):
     story.append(Paragraph('  201   Produto 1                                         4     X     13,50        54.00'
                            .replace(' ', '&nbsp;'), items))
 
 story.append(ReportLine())
-story.append(Paragraph('<b>Desconto R$ 0.00                              Valor total R$ 283.00</b>'
+story.append(Paragraph('Cartão                                                                     R$ 100.00', header_items))
+story.append(Paragraph('Cheque                                                                     R$ 100.00', header_items))
+story.append(Paragraph('Dinheiro                                                                   R$ 83.00', header_items))
+story.append(Paragraph('Desconto R$ 0.00                                <b>Valor total R$ 283.00</b>'
                        .replace(' ', '&nbsp;'), header_items))
 story.append(ReportLine())
 story.append(Paragraph('<b>Troco R$ 0.00                                     Valor pago R$ 283.00</b>'
@@ -123,6 +112,8 @@ story.append(Paragraph('<b>Troco R$ 0.00                                     Val
 story.append(ReportLine())
 story.append(Paragraph('Data 20/20/20                                                 Hora 10:10:10\n\n'
                        .replace(' ', '&nbsp;'), header_items))
+story.append(Paragraph('Venda:     #78455'.replace(' ', '&nbsp;'), header_items))
+story.append(Paragraph('Pedido:    #15'.replace(' ', '&nbsp;'), header_items))
 story.append(Paragraph('<b>VOLTE SEMPRE, SO JESUS SALVA</b>', footer))
 doc = MyDocTemplate('mintoc.pdf')
 doc.multiBuild(story)
