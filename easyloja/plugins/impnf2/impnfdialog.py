32b4d92167bab4c1845591a799fa810970c32d1a
marcelo <mmaia.cc@gmail.com>
2018-03-13 11:17:15 -0300

    teste impressao mudado;

--- easyloja/plugins/impnf2/impnfdialog.py
+++ easyloja/plugins/impnf2/impnfdialog.py
@@ -74,7 +74,7 @@ class ImpnfEditor(BaseEditor):
         self.image_default.set_from_stock(icon_dict.get(value), gtk.ICON_SIZE_BUTTON)
 
     def on_test_button__clicked(self, widget):
-        filename = os.path.join(get_application_dir(), 'stoq.conf')
+        filename = os.path.join(get_application_dir(), 'sale.pdf')
         self._print_on_spooler(filename)
 
 
