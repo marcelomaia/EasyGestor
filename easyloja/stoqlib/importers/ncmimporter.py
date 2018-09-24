b1c8cf274fe60ff7309c2db9a29eec0790d72735
marcelo <mmaia.cc@gmail.com>
2018-09-24 09:52:01 -0300

    resolução de bug no calculo de imposto quando a venda tem desconto.

--- easyloja/stoqlib/importers/ncmimporter.py
+++ easyloja/stoqlib/importers/ncmimporter.py
@@ -1,6 +1,7 @@
+import csv
+
 from kiwi.currency import currency
 from kiwi.environ import environ
-import csv
 
 csv_file = environ.find_resource('csv', 'ibpt.csv')
 
@@ -10,43 +11,50 @@ def ncm_table():
     rows = [row for row in f]
     ncm = {}
     for row in rows:
-        ncm[row[0]] = {'aliqNac': row[4], 'aliqImp':row[5]}
+        ncm[row[0]] = {'aliqNac': row[4], 'aliqImp': row[5]}
     return ncm
 
 
 def tax_on_cupom(sale):
-        ncm = ncm_table()
-        impostos = currency('0.00')
-        for item in sale.get_items():
-            sellable = item._get_sellable()
-            product = sellable._get_product()
-            if not product:
-                continue
+    ncm = ncm_table()
+    impostos = currency('0.00')
+    discount_value = sale.discount_value
+    subtotal = sale.get_total_sale_amount()
 
-            ncm_product = product.ncm
-            aliq = ncm.get(ncm_product)
-            if aliq is None:
-                continue
+    discount_percentage = float(discount_value) / float(subtotal)
 
-            if product.icms_template:
-                orig = getattr(product.icms_template, 'orig', 0)
-                if orig in (0, 3, 4, 5):
-                    impostos += currency(str(item.price)) * (currency(aliq['aliqNac'])) * item.quantity / 100
-                    print item.price, aliq['aliqNac'], impostos
-                else:
-                    impostos += currency(str(item.price)) * (currency(aliq['aliqImp'])) * item.quantity / 100
-                    print item.price, aliq['aliqImp'], impostos
+    for item in sale.get_items():
+        sellable = item._get_sellable()
+        product = sellable._get_product()
+        if not product:
+            continue
+
+        ncm_product = product.ncm
+        aliq = ncm.get(ncm_product)
+        if aliq is None:
+            continue
+
+        if product.icms_template:
+            orig = getattr(product.icms_template, 'orig', 0)
+            if orig in (0, 3, 4, 5):
+                calc = currency(str(item.price)) * (currency(aliq['aliqNac'])) * item.quantity / 100
+                calc = currency(calc) - currency(calc) * currency(discount_percentage)
+                impostos += calc
+                print item.price, aliq['aliqNac'], impostos, discount_percentage
             else:
-                continue
-            if sale.discount_value > 0:
-                # diminui a qtde de impostos de acordo com o desconto da venda
-                impostos -= impostos * sale.discount_value / sale.get_sale_subtotal()
-        tax_cupom = "Val Aprox Tributos R$ {:.2f} ({:.2f} %) Fonte- IBPT".format(impostos,
-                                                                                 (impostos*100)/sale.get_sale_subtotal()-sale.discount_value)
-        return tax_cupom.replace('.', ',')
+                calc = currency(str(item.price)) * (currency(aliq['aliqImp'])) * item.quantity / 100
+                calc = currency(calc) - currency(calc) * currency(discount_percentage)
+                impostos += calc
+                print item.price, aliq['aliqImp'], impostos, discount_percentage
+        else:
+            continue
+    tax_cupom = "Val Aprox Tributos R$ {:.2f} ({:.2f} %) " \
+                "Fonte: IBPT".format(impostos,
+                                     (impostos / sale.get_total_sale_amount() * 100))
+    return tax_cupom.replace('.', ',')
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     table = ncm_table()
     ncm = raw_input("NCM: ")
     print(table[ncm])
